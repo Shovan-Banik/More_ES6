49d318e6977add85948519120ae47fd330dbc500
Shovan-Banik <shovan15-7790@diu.edu.bd>
2023-02-21 00:25:27 +0600

    forEach loop explore

---
+++ forEach.js
@@ -0,0 +1,10 @@
+// forEach map ar motoi kaj kore...sudhu matro parthokko holo..map return kore...kinto forEach return kore na
+const coustomers=['abul','haji','masud','shahin','gias','hosen','josna','maoi','mohammod','fajlo'];
+// coustomers.forEach(n=>console.log(n));
+const khala=[
+    {name: 'shokhiton',ranna: 'valo',length: '1 year'},
+    {name: 'kolson',ranna: 'motamoti',length: '3 year'},
+    {name: 'mota',ranna: 'valo',length: '4 month'},
+    {name: 'borishailla',ranna: 'baje',length: '3 month'},
+]
+khala.forEach(n=>console.log(n.name,n.ranna));
